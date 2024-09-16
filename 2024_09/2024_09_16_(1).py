e802f133c75bba1e7ee073cbf0e0bcc94451823e
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-09-16 23:08:33 +0900

    2024_09_16_success

---
+++ 2024_09/2024_09_16_(1).py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/24262
+
+
+# 알고리즘 
+def MenOfPassion(n) : 
+    # 항상 코드는 1번만 수행된다. 
+    print("1")
+    print("0")
+
+
+# 입력받기
+n = int(input())
+
+
+# 함수 실행
+MenOfPassion(n)
