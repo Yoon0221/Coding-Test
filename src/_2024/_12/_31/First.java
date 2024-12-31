87cd93cfd42a850569d27946062e1ae30003b6a2
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-12-31 17:45:19 +0900

    [Feat] 2024_12_31

---
+++ src/_2024/_12/_31/First.java
@@ -0,0 +1,14 @@
+package _2024._12._31;
+
+// https://www.acmicpc.net/problem/11654
+
+import java.io.*;
+
+public class First {
+
+    static BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+    public static void main (String[] args) throws IOException {
+        System.out.println(br.readLine().charAt(0)-'0'+48);
+    }
+}
